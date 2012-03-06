df5e9410b34ba0f7b67e6dd913d48b491b9d0e00
ding xiao <ding@perfectsensedigital.com>
2012-03-06 11:04:42 -0500

    use custom color for button

--- SlideInTablelayout/gen/com/psd/android/R.java
+++ SlideInTablelayout/gen/com/psd/android/R.java
@@ -13,54 +13,72 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue2=0x7f050006;
+        public static final int blue25=0x7f050007;
+        public static final int grey05=0x7f050008;
+        public static final int grey15=0x7f050009;
+        public static final int grey25=0x7f05000a;
+        public static final int orange4=0x7f050002;
+        public static final int orange5=0x7f050003;
+        public static final int red4=0x7f050004;
+        public static final int red5=0x7f050005;
+        public static final int white1=0x7f05000b;
+        public static final int white2=0x7f05000c;
+        public static final int yellow1=0x7f050000;
+        public static final int yellow2=0x7f050001;
+    }
     public static final class drawable {
-        public static final int dl_01=0x7f020000;
-        public static final int dl_02=0x7f020001;
-        public static final int dl_03=0x7f020002;
-        public static final int dl_04=0x7f020003;
-        public static final int game1=0x7f020004;
-        public static final int game2=0x7f020005;
-        public static final int game3=0x7f020006;
-        public static final int game4=0x7f020007;
-        public static final int game5=0x7f020008;
-        public static final int game6=0x7f020009;
-        public static final int game7=0x7f02000a;
-        public static final int game8=0x7f02000b;
-        public static final int game9=0x7f02000c;
-        public static final int ic_launcher=0x7f02000d;
-        public static final int ic_tab_hiddenobject=0x7f02000e;
-        public static final int ic_tab_new=0x7f02000f;
-        public static final int ic_tab_puzzle=0x7f020010;
-        public static final int ic_tab_top20=0x7f020011;
-        public static final int iplay_logo=0x7f020012;
-        public static final int star_big_on=0x7f020013;
-        public static final int stat_happy=0x7f020014;
-        public static final int stat_neutral=0x7f020015;
-        public static final int stat_sad=0x7f020016;
-        public static final int stat_sample=0x7f020017;
+        public static final int black_small_button=0x7f020000;
+        public static final int custom_button=0x7f020001;
+        public static final int dl_01=0x7f020002;
+        public static final int dl_02=0x7f020003;
+        public static final int dl_03=0x7f020004;
+        public static final int dl_04=0x7f020005;
+        public static final int game1=0x7f020006;
+        public static final int game2=0x7f020007;
+        public static final int game3=0x7f020008;
+        public static final int game4=0x7f020009;
+        public static final int game5=0x7f02000a;
+        public static final int game6=0x7f02000b;
+        public static final int game7=0x7f02000c;
+        public static final int game8=0x7f02000d;
+        public static final int game9=0x7f02000e;
+        public static final int ic_launcher=0x7f02000f;
+        public static final int ic_tab_hiddenobject=0x7f020010;
+        public static final int ic_tab_new=0x7f020011;
+        public static final int ic_tab_puzzle=0x7f020012;
+        public static final int ic_tab_top20=0x7f020013;
+        public static final int iplay_logo=0x7f020014;
+        public static final int red_small_button=0x7f020015;
+        public static final int star_big_on=0x7f020016;
+        public static final int stat_happy=0x7f020017;
+        public static final int stat_neutral=0x7f020018;
+        public static final int stat_sad=0x7f020019;
+        public static final int stat_sample=0x7f02001a;
     }
     public static final class id {
-        public static final int button10=0x7f06000b;
-        public static final int button4=0x7f060005;
-        public static final int button5=0x7f060006;
-        public static final int button6=0x7f060007;
-        public static final int button7=0x7f060008;
-        public static final int button8=0x7f060009;
-        public static final int button9=0x7f06000a;
-        public static final int mySelection=0x7f060002;
-        public static final int puzzleGallery=0x7f06000c;
-        public static final int puzzleGameGallery=0x7f06000e;
-        public static final int puzzleRecommend=0x7f06000d;
-        public static final int searchButton=0x7f060004;
-        public static final int searchText=0x7f060003;
-        public static final int tableLayout1=0x7f060000;
-        public static final int test_image=0x7f060001;
+        public static final int button10=0x7f07000b;
+        public static final int button4=0x7f070005;
+        public static final int button5=0x7f070006;
+        public static final int button6=0x7f070007;
+        public static final int button7=0x7f070008;
+        public static final int button8=0x7f070009;
+        public static final int button9=0x7f07000a;
+        public static final int mySelection=0x7f070002;
+        public static final int puzzleGallery=0x7f07000c;
+        public static final int puzzleGameGallery=0x7f07000e;
+        public static final int puzzleRecommend=0x7f07000d;
+        public static final int searchButton=0x7f070004;
+        public static final int searchText=0x7f070003;
+        public static final int tableLayout1=0x7f070000;
+        public static final int test_image=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f060001;
+        public static final int hello=0x7f060000;
     }
 }
